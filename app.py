ae36871a0a78c514ba18d2d2a935ece4ec679770
Olusegun .O. ADELEKE <oadeleke123@gmail.com>
2024-07-18 00:22:33 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Dev54321\n!'
+    return 'Hello, Dev54321 we are the first test\n!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
